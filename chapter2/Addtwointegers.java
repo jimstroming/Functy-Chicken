4c19d39501ec14a9ad3cf257eb32c77a9f7d59ac
Jim Stroming <jim.stroming@ibm.com>
2017-04-06 17:28:13 -0500

    clean up a little

--- chapter2/Addtwointegers.java
+++ chapter2/Addtwointegers.java
@@ -2,11 +2,6 @@ import static java.lang.System.out;
 
 public class Addtwointegers {
 
-
-  static Function<Integer, Integer> triple = x -> x * 3;
-
-  static Function<Integer, Integer> square = x -> x * x;
-
   static Function<Integer, Function<Integer,Integer>> addtwo = x -> y -> x+y;
 
   public static void main(String[] args)  {
