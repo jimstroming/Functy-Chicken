5c544afc906fd3b58f346ccae8d58d66354ee25f
Jim Stroming <jim.stroming@ibm.com>
2017-04-06 13:09:42 -0500

    start writing the FunctionalMethods example

---
+++ chapter2/FunctionalMethods.java
@@ -0,0 +1,13 @@
+public class FunctionalMethods {
+   
+  public int percent1 = 5;
+  private int percent2 = 9;
+  public final int percent3 = 13;
+
+  public int  add(int a, int b) {
+    return a + b;
+  }
+
+}
+
+  
