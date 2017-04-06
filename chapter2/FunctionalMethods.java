d639658e481778b35a5edd104423e604d41c10d2
Jim Stroming <jim.stroming@ibm.com>
2017-04-06 13:43:55 -0500

    finish the example.

--- chapter2/FunctionalMethods.java
+++ chapter2/FunctionalMethods.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class FunctionalMethods {
    
   public int percent1 = 5;
@@ -8,6 +10,36 @@ public class FunctionalMethods {
     return a + b;
   }
 
+  public void setPercent2(int value) {
+    percent2 = value;
+  }
+
+  public int mult(int a, Integer b) {
+    a = 5;
+    b = 2;
+    return a * b;
+  }
+
+  public int div(int a, int b) {
+    return a / b;
+  }
+
+  public int applyTax1(int a) {
+    return a / 100 * (100 + percent1);
+  }
+
+  public int applyTax2(int a) {
+    return a / 100 * (100 + percent2);
+  }
+
+  public int applyTax3(int a) {
+    return a / 100 * (100 + percent3);
+  }
+
+  public List<Integer> append(int i, List<Integer> list) {
+    list.add(1);
+    return list;
+  }
 }
 
   
